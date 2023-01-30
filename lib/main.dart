529ed5de575848283555b4c9a3fe7c8679f83aba
Ron Wolf <RonaldBWolf@wolfamily.net>
2023-01-30 08:41:07 -0600

    first commit for sharing with work

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+import './question.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// Replaces standard main if function has exactly one statement.
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createStyate
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  num _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex++;
+    });
+    print(_questionIndex);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      "What's your favorite color?",
+      "What's your favorite animal?",
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Quiz Show!"),
+        ),
+        body: Column(children: [
+          Text(questions[_questionIndex]),
+          ElevatedButton(
+            child: Text('Answer 1'),
+            onPressed: _answerQuestion,
+          ),
+          ElevatedButton(
+            child: Text('Answer 2'),
+            onPressed: () => print('answer 2 chosen'),
+          ),
+          ElevatedButton(
+            child: Text('Answer 3'),
+            onPressed: () => print('answer 3 chosen'),
+          ),
+        ]),
+      ),
+    );
+  }
+}
